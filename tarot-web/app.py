ed26f4e7f8421e9e5a9ae1dbb5cb6d965ea980cf
juewangyipi <hari2926337588@gmail.com>
2026-05-08 22:06:35 +0800

    Update app.py

--- tarot-web/app.py
+++ tarot-web/app.py
@@ -717,4 +717,4 @@ def api_card_detail(card_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
